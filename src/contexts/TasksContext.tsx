bdfb3c7f5ecfc38e56796e2d3337c1fcba9c5dd0
Diego Natalo <diegonatalo@gmail.com>
2023-01-12 23:48:01 -0300

    feat: change { tasks: [] } to []

--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -25,7 +25,7 @@ interface TasksProviderProps {
 const TASKS_STORAGE_KEY = 'toDo:tasks'
 
 export function TasksProvider({ children }: TasksProviderProps) {
-  const [tasksState, dispatch] = useReducer(TaskReducer, { tasks: [] }, () => {
+  const [tasksState, dispatch] = useReducer(TaskReducer, [], () => {
     const storedTasks = localStorage.getItem(TASKS_STORAGE_KEY)
 
     if (storedTasks) {
