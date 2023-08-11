3a00d4e4cf1c8b631060fbb697c138ea4ec48671
Diego Natalo <diegonatalo@gmail.com>
2023-08-11 14:32:54 -0300

    remove SQlite and change for Vercel Postgres

--- prisma/migrations/20230811172933_init/migration.sql
+++ prisma/migrations/20230811172933_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "isDone" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "task_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
